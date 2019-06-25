3f65c56e5d08a5d26aa847395e1e92485a9a7661
laoyang <super9208@live.cn>
2019-06-25 15:21:47 +0800

    2. Vue.js通过axios访问Flask RESTful API设置flask跨域

--- back-end/app/__init__.py
+++ back-end/app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from config import Config
+from flask_cors import CORS
 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+    CORS(app)
 
     # 注册 blueprint
     from app.api import bp as api_bp
